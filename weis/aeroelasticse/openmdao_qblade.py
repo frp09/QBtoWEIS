fix AEP computation with DLC 1.3 & 1.6

weis/aeroelasticse/openmdao_qblade.py
@@ -33,7 +33,7 @@ import wisdem.commonse.utilities              as util
 from wisdem.rotorse.rotor_power             import eval_unsteady
 from wisdem.floatingse.floating_frame import NULL, NNODES_MAX, NELEM_MAX
 from weis.dlc_driver.dlc_generator    import DLCGenerator
-from weis.dlc_driver.dlc_generator    import DLCInstance
+from weis.dlc_driver.dlc_generator    import DLCInstance, get_dlc_label_for_aep
 from weis.aeroelasticse.CaseGen_General import CaseGen_General
 from functools import partial
 from pCrunch import PowerProduction
@@ -2032,20 +2032,12 @@ class QBLADELoadCases(ExplicitComponent):
 
         modopts = self.options['modeling_options']
         DLCs = [i_dlc['DLC'] for i_dlc in modopts['DLC_driver']['DLCs']]
-        if 'AEP' in DLCs:
-            DLC_label_for_AEP = 'AEP'
-        elif '1.1' in DLCs:
+        DLC_label_for_AEP = get_dlc_label_for_aep(DLCs)
+        if DLC_label_for_AEP is None:
             DLC_label_for_AEP = '1.1'
-            logger.warning('WARNING: DLC 1.1 is being used for AEP calculations.  Use the AEP DLC for more accurate wind modeling with constant TI.')
-        elif '1.3' in DLCs:
-            DLC_label_for_AEP = '1.3'
-            logger.warning('WARNING: DLC 1.3 is being used for AEP calculations.  Use the AEP DLC for more accurate wind modeling with constant TI.')
-        elif '1.6' in DLCs:
-            DLC_label_for_AEP = '1.6'
-            logger.warning('WARNING: DLC 1.6 is being used for AEP calculations.  Use the AEP DLC for more accurate wind modeling with constant TI.')
-        else: 
-            DLC_label_for_AEP = '1.1'
-            logger.warning('WARNING: DLC 1.1 is being used for AEP calculations.  Use the AEP DLC for more accurate wind modeling with constant TI.')
+            logger.warning('WARNING: No dedicated AEP-like DLC was found. Falling back to DLC 1.1 logic if matching cases exist.')
+        elif DLC_label_for_AEP != 'AEP':
+            logger.warning(f'WARNING: DLC {DLC_label_for_AEP} is being used for AEP calculations. Use the AEP DLC for more accurate wind modeling with constant TI.')
 
         if self.qb_vt['QSim']['DLCGenerator']:
             idx_pwrcrv = []
@@ -2060,7 +2052,7 @@ class QBLADELoadCases(ExplicitComponent):
 
             if len(failed_sim_ids) > 0:
                 mask = ~np.isin(idx_pwrcrv, failed_sim_ids)
-                idx_pwrcrv = np.arange(len(idx_pwrcrv[mask]))
+                idx_pwrcrv = idx_pwrcrv[mask]
                 U = U[mask]
 
                 print("U:", U)

weis/glue_code/gc_LoadInputs.py
@@ -7,7 +7,7 @@ from rosco import discon_lib_path
 import weis.inputs as sch
 from openfast_io.FAST_reader import InputReader_OpenFAST
 from wisdem.glue_code.gc_LoadInputs import WindTurbineOntologyPython
-from weis.dlc_driver.dlc_generator    import DLCGenerator
+from weis.dlc_driver.dlc_generator    import DLCGenerator, get_dlc_label_for_aep
 from openmdao.utils.mpi import MPI
 from rosco.toolbox.inputs.validation import load_rosco_yaml
 from wisdem.inputs import load_yaml
@@ -287,13 +287,12 @@ class WindTurbineOntologyPythonWEIS(WindTurbineOntologyPython):
             dlc_generator.generate(DLCopt['DLC'], DLCopt)
         self.modeling_options['DLC_driver']['n_cases'] = dlc_generator.n_cases
         
-        # Determine wind speeds that will be used to calculate AEP (using DLC AEP or 1.1)
+        # Determine wind speeds that will be used to calculate AEP.
+        # Prefer the dedicated AEP DLC, but allow operating DLC fallbacks when
+        # users intentionally estimate AEP from those cases.
         DLCs = [i_dlc['DLC'] for i_dlc in self.modeling_options['DLC_driver']['DLCs']]
-        if 'AEP' in DLCs:
-            DLC_label_for_AEP = 'AEP'
-        else:
-            DLC_label_for_AEP = '1.1'
-        dlc_aep_ws = [c.URef for c in dlc_generator.cases if c.label == DLC_label_for_AEP]
+        DLC_label_for_AEP = get_dlc_label_for_aep(DLCs)
+        dlc_aep_ws = [c.URef for c in dlc_generator.cases if c.label == DLC_label_for_AEP] if DLC_label_for_AEP else []
         self.modeling_options['DLC_driver']['n_ws_aep'] = len(np.unique(dlc_aep_ws))
 
         # TMD modeling